ad0bc1336b4d02a567e470fdfd654b5f509aa2ef
gregpaton08 <gregpaton08@gmail.com>
2017-05-28 22:28:51 -0400

    Display inside temperature above outside temperature

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 import get_weather
+import thermometer
 
 @app.route('/')
 @app.route('/index')
@@ -8,5 +9,6 @@ def index():
     # get forecast for next 12 hours
     hourly_forecast = get_weather.get_hourly_forecast()[:12]
     return render_template('index.html',
+                           inside_temperature=round(thermometer.read_temp_f(), 1),
                            temperature=get_weather.get_temperature(),
                            hourly=hourly_forecast)
---
+++ thermometer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os
+import time
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+temp_sensor = '/sys/bus/w1/devices/28-000004abf982/w1_slave'
+
+def read_sensor():
+    with open(temp_sensor, 'r') as file:
+        lines = file.readlines()
+    return lines
+
+
+def read_temp():
+    data = read_sensor()
+    while 'YES' not in data[0]:
+        time.sleep(0.2)
+        data = read_sensor()
+    temperature = data[1][data[1].find('=') + 1:]
+    temperature = float(temperature) / 1000.0
+    return temperature
+
+
+def read_temp_f():
+    return convert_celsius_to_fahrenheit(read_temp())
+
+
+def convert_celsius_to_fahrenheit(celsius):
+    return celsius * 9.0 / 5.0 + 32.0
+
+
+if __name__ == '__main__':
+    temperature = read_temp()
+    print(temperature)
+    print(convert_celsius_to_fahrenheit(temperature))
