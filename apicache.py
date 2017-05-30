e817238eae655bb129f0ca5da394b1e6204976f7
gregpaton08 <gregpaton08@gmail.com>
2017-05-30 00:35:46 -0400

    Add timestamp to cache

--- apicache.py
+++ apicache.py
@@ -2,6 +2,7 @@
 
 import pickle
 import os
+import datetime
 
 api_cache_file = 'apicache.p'
 
@@ -17,10 +18,17 @@ def __load_cache_data():
 
 def save_cache_data(key, data):
     cache_data = __load_cache_data()
-    cache_data[key] = data
+    cache_data[key] = {
+        'data' : data,
+        'time' : datetime.datetime.now()
+    }
     pickle.dump(cache_data, open(api_cache_file, 'wb'))
 
 
 def get_cache_data(key):
     cache_data = __load_cache_data()
-    return cache_data.get(key, None)
+    if not cache_data.get(key, None):
+        return None
+    time = cache_data[key].get('time', None)
+    print("{0} second".format((datetime.datetime.now() - time).seconds))
+    return cache_data[key].get('data', {})
