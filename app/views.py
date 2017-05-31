5524bb18f8be541c12979defbcf700dcf48f5ce9
gregpaton08 <gregpaton08@gmail.com>
2017-05-30 21:11:19 -0400

    Display weather conditions in lower case

--- app/views.py
+++ app/views.py
@@ -8,9 +8,8 @@ import thermometer
 def index():
     # get forecast for next 12 hours
     hourly_forecast = get_weather.get_hourly_forecast(12)
-    print(len(hourly_forecast))
     return render_template('index.html',
                            inside_temperature=round(thermometer.read_temp_f(), 0),
                            outside_temperature=round(get_weather.get_temperature(), 0),
-                           condition=get_weather.get_condition(),
+                           condition=get_weather.get_condition().lower(),
                            hourly=hourly_forecast)
