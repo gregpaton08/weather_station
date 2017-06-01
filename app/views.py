5058d83f6916243e00048895e5d856a470265f12
gregpaton08 <gregpaton08@gmail.com>
2017-05-31 23:53:16 -0400

    Add server side function to get inside temperature

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, jsonify
 from app import app
 import get_weather
 import thermometer
@@ -13,3 +13,8 @@ def index():
                            outside_temperature=round(get_weather.get_temperature(), 0),
                            condition=get_weather.get_condition().lower(),
                            hourly=hourly_forecast)
+
+
+@app.route('/get_inside_temperature')
+def get_inside_temperature():
+    return jsonify(round(thermometer.read_temp_f(), 0))
