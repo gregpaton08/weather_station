d6697833f63eb837c3bb6b3fd8574843f4e06ebd
gregpaton08 <gregpaton08@gmail.com>
2017-06-12 11:08:46 -0400

    Close database connection when finished

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
-from flask import Flask
+from flask import Flask, g
 
 app = Flask(__name__)
 from app import views
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
