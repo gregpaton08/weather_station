3a9b31c19ad02e69fc09bb879b807809316b789d
gregpaton08 <gregpaton08@gmail.com>
2017-06-12 08:56:49 -0400

    Add function to check if tables exists in database

--- track_thermometer.py
+++ track_thermometer.py
@@ -4,17 +4,27 @@ import sqlite3
 import time
 import thermometer
 
-conn = sqlite3.connect('test.db')
 
-time = int(round(time.time(), 0))
-temperature = int(thermometer.read_temp_c() * 100)
-sqlite_command = 'INSERT INTO INDOOR_TEMPERATURE (TIME, TEMPERATURE) VALUES ({0}, {1});'.format(time, temperature)
-print(sqlite_command)
-conn.execute(sqlite_command)
-conn.commit()
+def does_table_exist(connection, table_name):
+    cursor = connection.execute('SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'{0}\';'.format(table_name))
+    result = cursor.fetchone()
+    return result is not None and table_name == result[0]
 
-cursor = conn.execute('SELECT * FROM INDOOR_TEMPERATURE')
-for row in cursor:
-    print(row)
 
-conn.close()
+if __name__ == '__main__':
+    conn = sqlite3.connect('test.db')
+
+    # time = int(round(time.time(), 0))
+    # temperature = int(thermometer.read_temp_c() * 100)
+    # sqlite_command = 'INSERT INTO INDOOR_TEMPERATURE (TIME, TEMPERATURE) VALUES ({0}, {1});'.format(time, temperature)
+    # print(sqlite_command)
+    # conn.execute(sqlite_command)
+    # conn.commit()
+
+    print does_table_exist(conn, 'INDOOR_TEMPERATURE')
+
+    cursor = conn.execute('SELECT * FROM INDOOR_TEMPERATURE')
+    for row in cursor:
+        print(row)
+
+    conn.close()
