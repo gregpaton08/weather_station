1815cce8d2d9ad75c6d0d69da6e5117ba4d2a049
gregpaton08 <gregpaton08@gmail.com>
2017-06-13 19:44:02 -0400

    Rely on AJAX to populate weather info on page

--- app/views.py
+++ app/views.py
@@ -10,8 +10,6 @@ def index():
     # get forecast for next 12 hours
     hourly_forecast = get_weather.get_hourly_forecast(12)
     return render_template('index.html',
-                           inside_temperature=round(thermometer.read_temp_f(), 0),
-                           outside_temperature=round(get_weather.get_temperature_f(), 0),
                            condition=get_weather.get_condition().lower(),
                            hourly=hourly_forecast)
 
