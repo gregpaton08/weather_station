853c7a0f0a5be095af3e95288188f3b5b6c36046
gregpaton08 <gregpaton08@gmail.com>
2017-08-24 23:37:43 -0400

    TESTING

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 
-from app import app
-
-if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=4000, debug=False)
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'Yo, it''s working!'
+if __name__ == "__main__":
     app.run()
