63673863ceacce06eaf774a835551ac051997b01
gregpaton08 <gregpaton08@gmail.com>
2017-08-24 23:35:59 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,6 @@
-#!flask/bin/python
-# from app import app
 
-# if __name__ == '__main__':
-#     #app.run(host='0.0.0.0', port=4000, debug=False)
-#     app.run(host='0.0.0.0', debug=False)
+from app import app
 
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return 'Yo, it''s working!'
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    #app.run(host='0.0.0.0', port=4000, debug=False)
     app.run()
