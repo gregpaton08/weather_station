550727bdc4287e2a008a4e458fd2b9ffa61a7429
gregpaton08 <gregpaton08@gmail.com>
2017-09-01 00:00:34 -0400

    Update post.sh to test production server

--- post.sh
+++ post.sh
@@ -1,1 +1,3 @@
-curl -u gpaton:$(cat api_password.txt) -i -H "Content-Type: application/json" -X POST -d '{"temperature":"21.3","time":"2017-08-31 23:37"}'  http://localhost:8000/update_temperature
+# curl -u gpaton:$(cat api_password.txt) -i -H "Content-Type: application/json" -X POST -d '{"temperature":"21.3"}'  http://localhost:8000/update_temperature
+
+curl -u gpaton:$(cat api_password.txt) -i -H "Content-Type: application/json" -X POST -d '{"temperature":"25.3","time":" "}'  https://murmuring-caverns-91180.herokuapp.com/update_temperature
