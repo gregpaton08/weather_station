14877b06e2998cfdc6fcacc92d6370f10c88fc30
gregpaton08 <gregpaton08@gmail.com>
2017-09-05 18:14:55 -0400

    Update post script to send current temperature to server

--- post_temperature.sh
+++ post_temperature.sh
@@ -1,3 +1,1 @@
-# curl -u gpaton:$(cat api_password.txt) -i -H "Content-Type: application/json" -X POST -d '{"temperature":"21.3"}'  http://localhost:8000/update_temperature
-
-curl -u gpaton:$(cat api_password.txt) -i -H "Content-Type: application/json" -X POST -d '{"temperature":"25.3","time":" "}'  https://murmuring-caverns-91180.herokuapp.com/update_temperature
+curl -u gpaton:$(cat api_password.txt) -i -H "Content-Type: application/json" -X POST -d '{"temperature":'"${temperature}"'}' https://murmuring-caverns-91180.herokuapp.com/update_temperature
