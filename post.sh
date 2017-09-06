f7f6c215411e4c96eb8edeabe81d93cded749831
gregpaton08 <gregpaton08@gmail.com>
2017-09-05 21:34:25 -0400

    Remove unused code

--- post.sh
+++ post.sh
@@ -1,6 +1,3 @@
 # curl -u gpaton:$(cat api_password.txt) -i -H "Content-Type: application/json" -X POST -d '{"temperature":"21.3"}'  http://localhost:8000/update_temperature
 
-temperature="$(./thermometer.py)"
-echo "${temperature}"
-echo '{"temperature":'"${temperature}"'}'
 curl -u gpaton:$(cat api_password.txt) -i -H "Content-Type: application/json" -X POST -d '{"temperature":'"23"'}' https://murmuring-caverns-91180.herokuapp.com/update_temperature
