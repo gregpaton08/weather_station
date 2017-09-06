194d0314e8b0cd4425bd6276bd349aa678feb5fd
gregpaton08 <gregpaton08@gmail.com>
2017-09-06 19:07:24 -0400

    Add logging to script to update temperature

--- post_temperature.sh
+++ post_temperature.sh
@@ -1,2 +1,4 @@
 temperature=$(./thermometer.py)
 curl -u gpaton:$(cat api_password.txt) -i -H "Content-Type: application/json" -X POST -d '{"temperature":'"${temperature}"'}' https://murmuring-caverns-91180.herokuapp.com/update_temperature
+
+echo 'updated temperature '${temperature} >> log.txt
