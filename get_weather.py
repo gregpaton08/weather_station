d88bcabd3abef7544e15ad117adfc1bd8996545c
gregpaton08 <gregpaton08@gmail.com>
2017-11-09 19:05:53 -0500

    Add documentation

--- get_weather.py
+++ get_weather.py
@@ -16,6 +16,11 @@ API_URL_LOCATION = 'NJ/Collingswood.json'
 
 
 def get_api_key():
+    """
+    Retrive the Weather Underground API key.
+
+    :returns: API key, or empty string on failure.
+    """
     key = os.environ.get('WU_API_KEY', None)
     if key:
         return key
@@ -25,6 +30,12 @@ def get_api_key():
 
 
 def __get_data_for_url_path(url_path):
+    """
+    Get the data for a given Weather Underground API URL.
+
+    :param url_path: URL path to retrieve data from. Documentation: https://www.wunderground.com/weather/api/d/docs?d=data/index
+    :returns: The data for the given URL.
+    """
     cache_data = apicache.get_cache_data(url_path)
     if cache_data is not None:
         return cache_data
@@ -39,24 +50,50 @@ def __get_data_for_url_path(url_path):
 
 
 def __get_current_observation_for_key(key):
+    """
+    Get the current weather information for a given key.
+
+    :param key: The key to retrive data for. i.e. 'temp_c' for the current temperature in celsius.
+    :returns: The data for the given key.
+    """
     url_path = '/conditions/q/' + API_URL_LOCATION
     weather_data = __get_data_for_url_path(url_path)
     return weather_data.get('current_observation', {}).get(key, None)
 
 
 def get_temperature_f():
+    """
+    Get the current temperature from Weather Underground in fahrenheit.
+
+    :returns: The current temperature in fahrenheit.
+    """
     return __get_current_observation_for_key('temp_f')
 
 
 def get_temperature_c():
+    """
+    Get the current temperature from Weather Underground in celsius.
+
+    :returns: The current temperature in celsius.
+    """
     return __get_current_observation_for_key('temp_c')
 
 
 def get_condition():
+    """
+    Get the current weather conditions from Weather Underground.
+
+    :returns: The current weather conditions.
+    """
     return __get_current_observation_for_key('weather')
 
 
 def get_hourly_forecast(num_hours=-1):
+    """
+    Get the hourly weather forecast.
+
+    :returns: The hourly weather forecast as a list of dicts.
+    """
     url_path = '/hourly/q/' + API_URL_LOCATION
     weather_data = __get_data_for_url_path(url_path)
     data = weather_data.get('hourly_forecast', [])[:num_hours]
@@ -82,6 +119,11 @@ def get_hourly_forecast(num_hours=-1):
 
 
 def get_hourly_history():
+    """
+    Get the hourly weather history.
+
+    :returns: The hourly weather history as a list of dicts.
+    """
     current_time = local_time.get_est_time()
 
     # Combining API calls resulted in missing data, so split into two requests.
@@ -108,20 +150,40 @@ def get_hourly_history():
 
 
 def get_hourly_weather():
+    """
+    Get both the hourly forecast and hourly history.
+
+    :returns: The hourly forecast and history.
+    """
     return get_hourly_forecast() + get_hourly_history()
 
 
 def __get_astronomy_data():
+    """
+    Get the astronomy data which includes time of sunrise and sunset.
+
+    :returns: The astronomy data.
+    """
     url_path = '/astronomy/q/' + API_URL_LOCATION
     return __get_data_for_url_path(url_path)
 
 
 def get_sunrise():
+    """
+    Get the time of sunrise.
+
+    :returns: The time of sunrise.
+    """
     data = __get_astronomy_data();
     return data['sun_phase']['sunrise']
 
 
 def get_sunset():
+    """
+    Get the time of sunset.
+
+    :returns: The time of sunset.
+    """
     data = __get_astronomy_data();
     return data['sun_phase']['sunset']
 
